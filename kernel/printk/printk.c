64d996868f82f8e9be8a2dddda58c7b2285fab39
Ipin22 <ucihamala573@gmail.com>
2025-04-25 05:50:43 +0000

    Revert "Printk: silence some spam log"
    This reverts commit 615a1fdf18c508b24874f70ec00fcba20b2e1e0d.

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2390,21 +2390,6 @@ EXPORT_SYMBOL_GPL(vprintk_default);
     }
     // =============================
 
-
-    // ====== FILTER SPAM LOG ======
-    const char *filter_list[] = {
-        "[Power/PPM]",
-        "[wlan]",
-        "kalPerMonHandler",
-        NULL
-    };
-    for (int i = 0; filter_list[i]; i++) {
-        if (strstr(textbuf, filter_list[i])) {
-            return 0; // skip log
-        }
-    }
-    // =============================
-
  */
 asmlinkage __visible int printk(const char *fmt, ...)
 {
